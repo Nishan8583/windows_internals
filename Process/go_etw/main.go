fb2d358dc3b3370cb49e89213ac75c4fe2ae0a74
nishan8583 <nishanmaharjan17@gmail.com>
2026-01-21 07:09:51 -0800

    more morning coding, took more time understanding actually lol

--- Process/go_etw/main.go
+++ Process/go_etw/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"syscall"
+	"unsafe"
 )
 
 const (
@@ -37,12 +38,70 @@ var SystemControlGuid = syscall.GUID{
 	Data4: [8]byte{0x9a, 0x82, 0x00, 0x60, 0x08, 0xa8, 0x69, 0x39},
 }
 
+// https://learn.microsoft.com/en-us/windows/win32/etw/wnode-header
+type WNODE_HEADER struct {
+	BufferSize        uint32
+	ProviderId        uint32
+	HistoricalContext uint64
+	TimeStamp         int64
+	Guid              syscall.GUID
+	ClientContext     uint32
+	Flags             uint32
+}
+
+// U can find the original C struct defnition in
+// https://learn.microsoft.com/en-us/windows/win32/api/evntrace/ns-evntrace-event_trace_properties
+// or in the path mentioned above for evntrace.h
+type EVENT_TRACE_PROPERTIES struct {
+	Wnode               WNODE_HEADER
+	BufferSize          uint32
+	MinimumBuffers      uint32
+	MaximumBuffers      uint32
+	MaximumFileSize     uint32
+	LogFileMode         uint32
+	FlushTimer          uint32
+	EnableFlags         uint32
+	AgeLimit            int32
+	NumberOfBuffers     uint32
+	FreeBuffers         uint32
+	EventsLost          uint32
+	BuffersWritten      uint32
+	LogBuffersLost      uint32
+	RealTimeBuffersLost uint32
+	LoggerThreadId      uintptr
+	LogFileNameOffset   uint32
+	LoggerNameOffset    uint32
+}
+
 func main() {
 	sessionName, err := syscall.UTF16FromString("NT Kernel Logger")
 	if err != nil {
 		fmt.Println("Error creating session name:", err)
 		return
 	}
-
+	logFile, err := syscall.UTF16FromString("test.log")
+	if err != nil {
+		fmt.Println("Error creating log file name:", err)
+		return
+	}
+	_ = logFile
 	_ = sessionName
+
+	// Windows expectes UTF16
+	// UTF-16 uses 2 bytes per code unit, i.e. each character/Code unit is 2 bytes
+	// Example:
+	// utf16Name := []uint16{'A', 'B', 0}
+	// 41 00  42 00  00 00
+	//^---^  ^---^  ^---^
+	// 2 B     2 B    2 B
+	// len(utf16Name) = 3 (nuumber of code units)
+	// Actual memory used is 6 bytes not 3
+	// ETW needs to know 6, not 3
+	// ETW reads memory like this:
+	// base_pointer + LoggerNameOffset → WCHAR*
+	// If you forget *2:
+	// Offsets point into the middle of data
+	bufferSize := uint32(
+		unsafe.Sizeof(EVENT_TRACE_PROPERTIES{}) + uintptr(len(sessionName)*2) + uintptr(len(logFile)*2),
+	)
 }
